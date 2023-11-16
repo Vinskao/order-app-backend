ff2a8e35403da2a679866082c2d2efed4585ed71
Tianyi Kao <59313635+Vinskao@users.noreply.github.com>
2023-11-16 08:22:14 -0600

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,15 +1,6 @@
-USE ProjectOrderNow;
--- Create Table orders
-CREATE TABLE orders (
-  id INT PRIMARY KEY,
-  customerId INT FOREIGN KEY REFERENCES customers(id),
-  isOrdered BIT,
-  created_at TIMESTAMP
-);
-
 -- Create Table customers
 CREATE TABLE customers (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY IDENTITY(1,1),
   username VARCHAR(20) UNIQUE,
   email NVARCHAR(50) UNIQUE,
   password NVARCHAR(20),
@@ -18,40 +9,16 @@ CREATE TABLE customers (
   created_at TIMESTAMP
 );
 
--- Create table orderToMeal
-CREATE TABLE orderToMeal (
-  orderId INT PRIMARY KEY,
-  meal NVARCHAR(20) PRIMARY KEY,
-  quantity INT,
-  FOREIGN KEY (orderId) REFERENCES orders(id),
-  FOREIGN KEY (meal) REFERENCES meal(id)
-);
-
--- Create Table meal
-CREATE TABLE meal (
-  id INT PRIMARY KEY,
-  name NVARCHAR(50),
-  price INT,
-  type NVARCHAR(50) REFERENCES mealType(id),
-  isAvailable BIT,
-  created_at TIMESTAMP
-);
-
 -- Create Table mealType
 CREATE TABLE mealType (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY IDENTITY(1,1),
   name NVARCHAR(50),
   created_at TIMESTAMP
 );
 
--- Create Table reserveRecord
-CREATE TABLE reserveRecord (
-  id INT PRIMARY KEY,
-  customerId INT FOREIGN KEY REFERENCES customers(id),
-  tableNumber INT FOREIGN KEY REFERENCES tables(tableNumber),
-  reservationTime TIME,
-  isReserved BIT,
-  created_at TIMESTAMP
+-- Create Table coupons
+CREATE TABLE coupons (
+  couponId INT PRIMARY KEY IDENTITY(1,1)
 );
 
 -- Create Table tables
@@ -61,14 +28,47 @@ CREATE TABLE tables (
   reservedTime TIME
 );
 
--- Create Table coupons
-CREATE TABLE coupons (
-  couponId INT PRIMARY KEY
+-- Create Table meal
+CREATE TABLE meal (
+  id INT PRIMARY KEY IDENTITY(1,1),
+  name NVARCHAR(50),
+  price INT,
+  type INT REFERENCES mealType(id),
+  isAvailable BIT,
+  created_at TIMESTAMP
+);
+
+-- Create Table orders
+CREATE TABLE orders (
+  id INT PRIMARY KEY IDENTITY(1,1),
+  customerId INT FOREIGN KEY REFERENCES customers(id),
+  isOrdered BIT,
+  created_at TIMESTAMP
+);
+
+-- Create table orderToMeal
+CREATE TABLE orderToMeal (
+  orderId INT PRIMARY KEY,
+  meal INT, -- Use the same data type as meal.id
+  quantity INT,
+  FOREIGN KEY (orderId) REFERENCES orders(id),
+  FOREIGN KEY (meal) REFERENCES meal(id)
+);
+
+
+-- Create Table reserveRecord
+CREATE TABLE reserveRecord (
+  id INT PRIMARY KEY IDENTITY(1,1),
+  customerId INT FOREIGN KEY REFERENCES customers(id),
+  tableNumber INT FOREIGN KEY REFERENCES tables(tableNumber),
+  reservationTime TIME,
+  isReserved BIT,
+  created_at TIMESTAMP
 );
 
 -- Create Table couponToCustomer
 CREATE TABLE couponToCustomer (
-  customerId INT PRIMARY KEY FOREIGN KEY REFERENCES customers(id),
-  couponId INT PRIMARY KEY FOREIGN KEY REFERENCES coupons(couponId),
+  customerId INT FOREIGN KEY REFERENCES customers(id),
+  couponId INT FOREIGN KEY REFERENCES coupons(couponId),
   quantity INT
 );
