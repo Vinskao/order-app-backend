aaf973b53b7fccd68628c3a528b2a10b489498ef
Vinskao <tianyikao@gmail.com>
2023-12-28 14:13:58 +0800

    init

---
+++ src/main/java/com/team/order/functionorder/FunctionOrderApplication.java
@@ -0,0 +1,13 @@
+package com.team.order.functionorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/order/functionorder/FunctionOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.order.functionorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
